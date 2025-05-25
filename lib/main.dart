d169d9a641a58d1916d989a53277c359ede70d75
Ayush Singh <ayushsingh30082005@gmail.com>
2025-05-25 23:29:54 +0530

    Minor fixes.

--- lib/main.dart
+++ lib/main.dart
@@ -232,7 +232,7 @@ class StreakCard extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: streak.monthlyStreak[i]
                         ? Theme.of(context).colorScheme.primary
-                        : Theme.of(context).colorScheme.surfaceVariant,
+                        : Theme.of(context).colorScheme.surfaceContainerHighest,
                     borderRadius: BorderRadius.circular(4),
                   ),
                 );
