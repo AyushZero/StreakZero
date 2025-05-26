02518e2cde8926a86d2b2ba754f8e25b0fe8607c
Ayush Singh <ayushsingh30082005@gmail.com>
2025-05-26 22:08:17 +0530

    Remodel 2.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Water Tracker',
+      title: 'Watah',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
         textTheme: const TextTheme(
